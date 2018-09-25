8f8ffb25bbf85596ebf612deafc34da90173e6b2
Lupe <galejo@csumb.edu>
2018-09-25 12:59:51 -0500

    Add files via upload

---
+++ config/connect.php
@@ -0,0 +1,11 @@
+<?php
+
+    // Database: AWS RDS Credentials
+    $host                   = 'cst438.xxxxxxxxxxx.us-west-2.rds.amazonaws.com:3306';
+    $port                   = '3306';
+    $dbname                 = 'cst438'; 
+    $username               = 'xxxxxxx'; 
+    $password               = 'xxxxxxx';
+
+    $conn = mysqli_connect($host, $username, $password); 
+?>
