3ab8756345952e9deab12844c0a64c9fdb57feff
Lupe <galejo@csumb.edu>
2018-09-25 13:05:54 -0500

    Add files via upload

---
+++ inc/index_header.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html> 
+<head>
+
+	<!-- Meta -->
+	<meta charset="utf-8">
+	<meta name="keywords" content="HTML5 Template" />
+	<meta name="description" content="CST438 - Software Engineering Project">
+	<meta name="author" content="Logistic Solutions - Steven H. ">
+
+	<title>Logistic Cafe &amp; Grill</title>
+
+	<!-- Mobile Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- Favicon -->
+	<link rel="shortcut icon" href="images/favicon.png">
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="js/isotope/isotope.css">
+	<link rel="stylesheet" href="js/flexslider/flexslider.css">
+	<link rel="stylesheet" href="js/owl-carousel/owl.carousel.css">
+	<link rel="stylesheet" href="js/owl-carousel/owl.theme.css">
+	<link rel="stylesheet" href="js/owl-carousel/owl.transitions.css">
+	<link rel="stylesheet" href="js/superfish/css/megafish.css" media="screen">
+	<link rel="stylesheet" href="js/superfish/css/superfish.css" media="screen">
+	<link rel="stylesheet" href="js/pikaday/pikaday.css">
+	<link rel="stylesheet" href="css/settings-panel.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/light.css">
+    <link rel="stylesheet" hret="css/footer.css">
+	<link rel="stylesheet" href="css/responsive.css">
+
+	<!-- JS Font Script -->
+	<script src="http://use.edgefonts.net/bebas-neue.js"></script>
+    
+    <!-- Modernizer -->
+	<script src="js/modernizr.custom.js"></script>
+
+    <!-- Alert Box --> 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+</head>
+    
+<body class="multi-page">
+    
+    <div id="wrapper" class="wrapper">
+    
+	<!-- HEADER -->
+	<header id="header2">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3 col-xs-5 logo">
+					<a href="index.php"><img src="http://[::1]/cishop/assets/images/logo.png" class="img-responsive" alt=""/></a>
+				</div>
+				<div class="col-md-9 col-xs-7">
+					<div class="top-bar">
+						
+					</div>
+				</div>
+			</div>
+            
+            <?php
+                // Header Title 
+                echo "
+                <div class='page-header page_header text-center'>
+                    <h1> Logistic cafe <span style='color:#ef7c00'> &amp; </span> Grill </h1> <br />
+                    <p> Your neighborhood bistro</p>
+                </div>";
+            ?>
---
+++ inc/nav.php
@@ -0,0 +1,97 @@
+			<div class="menu-wrap">
+				<div id="mobnav-btn">Menu <i class="fa fa-bars"></i></div>
+				<ul class="sf-menu">
+					<li>
+						<a href="../index.php">Home</a>
+					</li>
+					<li>
+						<a href="#">Menus</a>
+						<div class="mobnav-subarrow"><i class="fa fa-plus"></i></div>
+						<ul>
+                            
+						<?php
+							$catsql = "SELECT * FROM category";
+							$catres = mysqli_query($connection, $catsql);
+							
+                            while($catr = mysqli_fetch_assoc($catres)) {
+						 ?>
+				                <li><a href="menu.php?id=<?php echo $catr['id']; ?>"><?php echo $catr['name']; ?></a></li>
+						<?php } ?>
+						</ul>
+					</li>
+					<li>
+						<a href="../contact.php">Contact</a>
+					</li>
+                    <li>
+						<a href="#">My Account</a>
+						<div class="mobnav-subarrow"><i class="fa fa-plus"></i></div>
+						<ul>
+							<li><a href="../my-account.php">My Orders</a></li>
+							<li><a href="../edit-address.php">Account Info</a></li>
+                            <li><a href="../login.php"> Login</a></li>
+							<li><a href="../logout.php">Logout</a></li>
+                            
+						</ul>
+					</li>
+				</ul>
+                
+				<div class="header-xtra">
+				
+                    <?php $cart = $_SESSION['cart']; ?>
+					
+                    <div class="s-cart">
+						
+                        <div class="sc-ico"><i class="fa fa-shopping-cart"></i><em><?php
+								echo count($cart); ?></em>
+                        </div>
+
+						<div class="cart-info">
+							<small>You have <em class="highlight"><?php
+								echo count($cart); ?> item(s)</em> in your shopping bag
+                            </small>
+							
+                            <br>
+							<br>
+							
+                            <?php
+				
+								$total = 0;
+				
+                                foreach ($cart as $key => $value) {
+									
+                                    $navcartsql = "SELECT * 
+                                                   FROM products 
+                                                   WHERE id=$key";
+                                    
+									$navcartres = mysqli_query($connection, $navcartsql);
+									$navcartr = mysqli_fetch_assoc($navcartres);
+                            ?>
+
+                                    <div class="ci-item">
+
+                                        <img src="admin/<?php echo $navcartr['thumb']; ?>" width="70" alt=""/>
+
+                                        <div class="ci-item-info">
+
+                                            <h5><a href="single.php?id=<?php echo $navcartr['id']; ?>"><?php echo substr($navcartr['name'], 0 , 20); ?></a></h5>
+                                            <p><?php echo $value['quantity']; ?> x $ <?php echo $navcartr['price']; ?></p>
+
+                                            <div class="ci-edit">
+                                                <a href="delcart.php?id=<?php echo $key; ?>" class="edit fa fa-trash"></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <?php $total = $total + ($navcartr['price']*$value['quantity']);
+                                } ?>
+							
+                            <div class="ci-total">Subtotal: $ <?php echo $total; ?></div>
+							
+                            <div class="cart-btn">
+								<a href="cart.php">View Cart</a> <a href="checkout.php">Checkout</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
